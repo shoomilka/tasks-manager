fd244e94215ce3047b7bae5ce513a0af1878c792
Ulyana Shoomilina <shoomilka@i.ua>
2020-02-15 14:47:10 +0200

    pagination added

--- public/js/app.js
+++ public/js/app.js
@@ -1899,6 +1899,79 @@ module.exports = {
 
 /***/ }),
 
+/***/ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/TasksComponent.vue?vue&type=script&lang=js&":
+/*!*************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/babel-loader/lib??ref--4-0!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/TasksComponent.vue?vue&type=script&lang=js& ***!
+  \*************************************************************************************************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ __webpack_exports__["default"] = ({
+  data: function data() {
+    return {
+      tasks: [],
+      task: {
+        id: '',
+        title: '',
+        description: ''
+      },
+      task_id: '',
+      pagination: {},
+      edit: false
+    };
+  },
+  created: function created() {
+    this.fetchTasks();
+  },
+  methods: {
+    fetchTasks: function fetchTasks(page_url) {
+      var _this = this;
+
+      page_url = page_url || '/api/tasks';
+      var vm = this;
+      fetch(page_url).then(function (responce) {
+        return responce.json();
+      }).then(function (responce) {
+        _this.tasks = responce.data;
+        var pagination = {
+          current_page: responce.current_page,
+          last_page: responce.last_page,
+          next_page_url: responce.next_page_url,
+          prev_page_url: responce.prev_page_url
+        };
+        _this.pagination = pagination;
+      })["catch"](function (error) {
+        return console.log(error);
+      });
+    }
+  }
+});
+
+/***/ }),
+
 /***/ "./node_modules/lodash/lodash.js":
 /*!***************************************!*\
   !*** ./node_modules/lodash/lodash.js ***!
@@ -19539,20 +19612,83 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _vm._m(0)
+  return _c(
+    "div",
+    {},
+    [
+      _c("h2", [_vm._v("Tasks")]),
+      _vm._v(" "),
+      _vm._l(_vm.tasks, function(task) {
+        return _c("div", { key: task.id, staticClass: "card card-body" }, [
+          _c("h3", [_vm._v(_vm._s(task.title))]),
+          _vm._v(" "),
+          _c("p", [_vm._v(_vm._s(task.description))])
+        ])
+      }),
+      _vm._v(" "),
+      _c(
+        "div",
+        { staticClass: "text-xs-center", attrs: { "aria-label": "" } },
+        [
+          _c(
+            "ul",
+            {
+              staticClass: "pagination",
+              staticStyle: { "padding-left": "45%" }
+            },
+            [
+              _vm.pagination.prev_page_url
+                ? _c("li", [
+                    _c(
+                      "a",
+                      {
+                        staticClass: "page-link",
+                        attrs: { href: "#" },
+                        on: {
+                          click: function($event) {
+                            return _vm.fetchTasks(_vm.pagination.prev_page_url)
+                          }
+                        }
+                      },
+                      [_vm._v("«")]
+                    )
+                  ])
+                : _vm._e(),
+              _vm._v(" "),
+              _c("li", { staticClass: "page-item" }, [
+                _c(
+                  "a",
+                  { staticClass: "page-link disabled", attrs: { href: "#" } },
+                  [_vm._v(_vm._s(_vm.pagination.current_page))]
+                )
+              ]),
+              _vm._v(" "),
+              _vm.pagination.next_page_url
+                ? _c("li", [
+                    _c(
+                      "a",
+                      {
+                        staticClass: "page-link",
+                        attrs: { href: "#" },
+                        on: {
+                          click: function($event) {
+                            return _vm.fetchTasks(_vm.pagination.next_page_url)
+                          }
+                        }
+                      },
+                      [_vm._v("»")]
+                    )
+                  ])
+                : _vm._e()
+            ]
+          )
+        ]
+      )
+    ],
+    2
+  )
 }
-var staticRenderFns = [
-  function() {
-    var _vm = this
-    var _h = _vm.$createElement
-    var _c = _vm._self._c || _h
-    return _c("div", { staticClass: "container" }, [
-      _c("a", { staticClass: "navbar-brand", attrs: { href: "#" } }, [
-        _vm._v("Task Manager")
-      ])
-    ])
-  }
-]
+var staticRenderFns = []
 render._withStripped = true
 
 
@@ -31850,15 +31986,17 @@ __webpack_require__.r(__webpack_exports__);
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _TasksComponent_vue_vue_type_template_id_8f69a68c___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./TasksComponent.vue?vue&type=template&id=8f69a68c& */ "./resources/js/components/TasksComponent.vue?vue&type=template&id=8f69a68c&");
-/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+/* harmony import */ var _TasksComponent_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./TasksComponent.vue?vue&type=script&lang=js& */ "./resources/js/components/TasksComponent.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
 
-var script = {}
+
+
 
 
 /* normalize component */
 
-var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_1__["default"])(
-  script,
+var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
+  _TasksComponent_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
   _TasksComponent_vue_vue_type_template_id_8f69a68c___WEBPACK_IMPORTED_MODULE_0__["render"],
   _TasksComponent_vue_vue_type_template_id_8f69a68c___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
   false,
@@ -31872,6 +32010,20 @@ var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_
 if (false) { var api; }
 component.options.__file = "resources/js/components/TasksComponent.vue"
 /* harmony default export */ __webpack_exports__["default"] = (component.exports);
+
+/***/ }),
+
+/***/ "./resources/js/components/TasksComponent.vue?vue&type=script&lang=js&":
+/*!*****************************************************************************!*\
+  !*** ./resources/js/components/TasksComponent.vue?vue&type=script&lang=js& ***!
+  \*****************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_TasksComponent_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/babel-loader/lib??ref--4-0!../../../node_modules/vue-loader/lib??vue-loader-options!./TasksComponent.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/TasksComponent.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport */ /* harmony default export */ __webpack_exports__["default"] = (_node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_TasksComponent_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
 
 /***/ }),
 
--- resources/js/components/TasksComponent.vue
+++ resources/js/components/TasksComponent.vue
@@ -1,6 +1,60 @@
 <template>
-    <div class="container">
-        <a href="#" class="navbar-brand">Task Manager</a>
+    <div class="">
+
+        <h2>Tasks</h2>
+
+        <div class="card card-body" v-for="task in tasks" v-bind:key="task.id">
+            <h3>{{ task.title }}</h3>
+            <p>{{ task.description }}</p>
+        </div>
+
+        <div aria-label="" class="text-xs-center">
+            <ul class="pagination" style="padding-left: 45%;">
+                <li v-if=pagination.prev_page_url><a class="page-link" href="#" @click="fetchTasks(pagination.prev_page_url)">&laquo;</a></li>
+                <li class="page-item"><a class="page-link disabled" href="#">{{ pagination.current_page }}</a></li>
+                <li v-if=pagination.next_page_url><a class="page-link" href="#" @click="fetchTasks(pagination.next_page_url)">&raquo;</a></li>
+            </ul>
+        </div>
     </div>
     
 </template>
+
+<script>
+    export default{
+        data(){
+            return {
+                tasks: [],
+                task:{
+                    id: '',
+                    title: '',
+                    description: ''
+                },
+                task_id: '',
+                pagination: {},
+                edit: false
+            }
+        },
+        created(){
+            this.fetchTasks();
+        },
+        methods:{
+            fetchTasks(page_url){
+                page_url = page_url || '/api/tasks'
+                let vm = this;
+                fetch(page_url)
+                    .then(responce => responce.json())
+                    .then(responce => {
+                        this.tasks = responce.data;
+                        let pagination = {
+                            current_page: responce.current_page,
+                            last_page: responce.last_page,
+                            next_page_url: responce.next_page_url,
+                            prev_page_url: responce.prev_page_url,
+                        };
+                        this.pagination = pagination;
+                    })
+                    .catch(error => console.log(error))
+            },
+        }
+    }
+</script>
